e665ef18e88fd220cb8802b7d2cb2837bb82ac15
Prabhakaran Kasinathan <prabhakaran.kasinathan@gmail.com>
2023-01-18 00:52:18 +0100

    fixed button price issue

--- js/_update_price.js
+++ js/_update_price.js
@@ -1,12 +1,11 @@
+
 window.addEventListener('DOMContentLoaded', function(event) {
     document.addEventListener('change', function(evt){
-
-        if(evt.target.classList.contains('qty')){
-            var button = evt.target.parentNode.querySelector('.buy-button')
+        console.log("event target",evt)
+        if(evt.target.attributes.price.value > 1){
+            var button = evt.target.childNode.querySelector('.buy-button')
             var qty = parseInt(evt.target.value)
-            var input = evt.target.parentNode.querySelector('.qty')
-            // console.log(input.getAttribute("price"))
-            price = parseInt(input.getAttribute("price"))
+            price = parseInt(evt.target.attributes.price.value)
             button.setAttribute('data-item-quantity', qty)
             var label = button.innerHTML
             label = "Add to cart (Total: INR "+ (qty * price)+")"
